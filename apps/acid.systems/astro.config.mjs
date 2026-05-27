9a5e2dd576e5b38f9974900fdf54022958c86132
Raúl Garcia <ego.war@icloud.com>
2026-05-27 15:50:28 -0700

    ✨ Sanity Astro
    Integrate Sanity Astro

--- apps/acid.systems/astro.config.mjs
+++ apps/acid.systems/astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import {defineConfig, fontProviders} from 'astro/config'
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   fonts: [
@@ -15,4 +18,11 @@ export default defineConfig({
       cssVariable: '--font-mono',
     },
   ],
+
+  integrations: [sanity({
+    projectId: 'nmhp3u9m',
+    dataset: 'production',
+    apiVersion: '2026-05-27',
+    useCdn: false,
+  }), react()],
 })
