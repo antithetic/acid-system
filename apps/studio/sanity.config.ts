56615b4eaaf0fb6f243c1ff48532993bc850c555
Raúl Garcia <ego.war@icloud.com>
2026-05-27 05:59:59 -0700

    ✨ Sanity
    cms

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,18 +1,1 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
-
-export default defineConfig({
-  name: 'default',
-  title: 'Acid System',
-
-  projectId: 'nmhp3u9m',
-  dataset: 'production',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
-})
+export {default} from '@repo/sanity'
