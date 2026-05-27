f9652d2be3afdf22200d9509d4e2ff028f58dc2a
Raúl Garcia <ego.war@icloud.com>
2026-05-27 04:47:46 -0700

    new layout

--- apps/acid.systems/astro.config.mjs
+++ apps/acid.systems/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import {defineConfig} from 'astro/config'
+import {defineConfig, fontProviders} from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: 'Instrument Serif',
+    cssVariable: '--font-serif',
+  }, {
+    provider: fontProviders.fontsource(),
+    name: 'JetBrains Mono',
+    cssVariable: '--font-mono',
+  }]
+})
