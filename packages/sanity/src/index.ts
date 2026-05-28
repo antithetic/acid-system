6407c32d09a97212c5b996b2face19a58883a147
Raúl Garcia <ego.war@icloud.com>
2026-05-27 23:32:45 -0700

    ✨ Sanity Tags
    Sanity Tags plugin for better visual categorization.

--- packages/sanity/src/index.ts
+++ packages/sanity/src/index.ts
@@ -2,8 +2,10 @@ import {visionTool} from '@sanity/vision'
 import {Cannabis} from 'lucide-react'
 import {defineConfig} from 'sanity'
 import {media} from 'sanity-plugin-media'
-import {references} from 'sanity-plugin-references'
+import {references, referencesView} from 'sanity-plugin-references'
 import {structureTool} from 'sanity/structure'
+import {tags} from 'sanity-plugin-tags-v4'
+
 import {webhooksTrigger} from 'sanity-plugin-webhooks-trigger'
 
 import {schemaTypes} from './schema'
@@ -17,11 +19,15 @@ export default defineConfig({
   icon: Cannabis,
 
   plugins: [
-    structureTool(),
+    structureTool({
+      defaultDocumentNode: (S) =>
+        S.document().views([S.view.form(), referencesView(S)]),
+    }),
     visionTool(),
     webhooksTrigger(),
     media(),
     references(),
+    tags(),
   ],
 
   schema: {
