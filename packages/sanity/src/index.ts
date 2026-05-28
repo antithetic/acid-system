dbafe2ffe527826220e1777aba19755d7ddaa10f
Raúl Garcia <ego.war@icloud.com>
2026-05-27 23:30:00 -0700

    ✨ Sanity References plugin
    plugin for easy access to all references in studio

--- packages/sanity/src/index.ts
+++ packages/sanity/src/index.ts
@@ -1,6 +1,8 @@
 import {visionTool} from '@sanity/vision'
+import {Cannabis} from 'lucide-react'
 import {defineConfig} from 'sanity'
 import {media} from 'sanity-plugin-media'
+import {references} from 'sanity-plugin-references'
 import {structureTool} from 'sanity/structure'
 import {webhooksTrigger} from 'sanity-plugin-webhooks-trigger'
 
@@ -12,8 +14,15 @@ export default defineConfig({
 
   projectId: 'nmhp3u9m',
   dataset: 'production',
+  icon: Cannabis,
 
-  plugins: [structureTool(), visionTool(), webhooksTrigger(), media()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    webhooksTrigger(),
+    media(),
+    references(),
+  ],
 
   schema: {
     types: schemaTypes,
